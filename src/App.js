a7734ffa13a821c7c2779fba241d052eb41fb79f
Abdulrasheedar <abdulrasheed.razaak@gmail.com>
2024-09-10 13:17:38 +0100

    Infinite scroll commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
-  pageSize = 4;
+  pageSize = 15;
   render() {
     return (
       <div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -15,22 +16,23 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  capFL = (string) =>{
-    return string.charAt(0).toUpperCase()+string.slice(1);
-  }
+  capFL = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults:0
     };
     document.title = `${this.capFL(this.props.category)} - NewsApp`;
   }
 
   async updateNews() {
-    let apiKey = 'abfca2cf565a403c92cbe98049d87e70';
+    let apiKey = "abfca2cf565a403c92cbe98049d87e70";
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -57,18 +59,41 @@ export class News extends Component {
     await this.updateNews();
   };
 
+  fetchMoreData = async () => {
+    this.setState({page:this.state.page + 1});
+    let apiKey = "abfca2cf565a403c92cbe98049d87e70";
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      // articles: parsedData.articles,
+      articles: this.state.articles.concat(parsedData.articles) ? this.state.articles.concat(parsedData.articles) : [],
+      // articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading:false
+    });
+  };
+
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center" style={{ margin: "40px 0px" }}>
           NewsApp - Top {this.capFL(this.props.category)} Headlines
         </h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={this.state.loading && <Spinner />}
+        >
+          <div className="container">
+          <div className="row">
+            {this.state.articles.map((element,index) => {
               return (
-                <div className="col-md-3" key={element.url}>
+                // <div className="col-md-3" key={element.url}>
+                <div className="col-md-3" key={index}>
                   <NewsItem
                     title={
                       element.title !== null
@@ -93,8 +118,11 @@ export class News extends Component {
                 </div>
               );
             })}
-        </div>
-        <div className="container d-flex justify-content-between">
+          </div>
+          </div>
+        </InfiniteScroll>
+        {/* Code for previous and Next buttons */}
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             type="button"
@@ -115,8 +143,8 @@ export class News extends Component {
           >
             Next &rarr;
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
